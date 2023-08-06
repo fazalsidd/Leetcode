76ae06e79c20c1fa69a67336e2e4d214b84729ea
fazalsidd <92528154+fazalsidd@users.noreply.github.com>
2023-08-06 15:43:35 +0530

    Time: 8 ms (44.12%), Space: 43.9 MB (19.48%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,25 +1,26 @@
 class Solution {
-    HashMap<String,Boolean>map=new HashMap<>();
-    public boolean helper(String s,List<String> word)
+    public boolean helper(String s, List<String> w,int l,int r,HashMap<String,Boolean> map)
     {
-        int len=s.length();
-        if(len==0)
-        return true;
-        if(map.containsKey(s))
-        return map.get(s);
-        boolean ans=false;
-        for(int i=1;i<=len;i++)
+        if(l>r)
+            return true;
+        
+        if(map.containsKey(s.substring(l,r+1)))
+            return map.get(s.substring(l,r+1));
+        
+        for(int i=l;i<=r;i++)
         {
-            if(word.contains(s.substring(0,i))&&helper(s.substring(i,len),word))
+            if(w.contains(s.substring(l,i+1))&&helper(s,w,i+1,r,map))
             {
-                ans=true;
-                break;
+                map.put(s.substring(l,r+1),true);
+                return true;
             }
         }
-         map.put(s,ans);
-         return ans;
+        
+        map.put(s.substring(l,r+1),false);
+        return false;
     }
-    public boolean wordBreak(String s, List<String> wordDict) {
-        return helper(s,wordDict);
+    public boolean wordBreak(String s, List<String> w) {
+        HashMap<String,Boolean> map=new HashMap<>();
+        return helper(s,w,0,s.length()-1,map);
     }
 }
