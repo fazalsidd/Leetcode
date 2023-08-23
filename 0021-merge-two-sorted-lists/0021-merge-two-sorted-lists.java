4b493562d95a99d7f1d8da6f31a729f3bfe42eba
fazalsidd <92528154+fazalsidd@users.noreply.github.com>
2023-08-23 22:24:00 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (71.39%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,36 +10,50 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode l=new ListNode(0);
-        ListNode ans=l;
+        if(l1==null)
+            return l2;
+        if(l2==null)
+            return l1;
+        ListNode ans=null;
+        if(l1.val<l2.val)
+        {
+            ans=l1;
+            l1=l1.next;
+        }
+        else
+        {
+            ans=l2;
+            l2=l2.next;
+        }
+        ListNode tra=ans;
         while(l1!=null&&l2!=null)
         {
             if(l1.val<l2.val)
-              { 
-                l.next=l1;
-                l=l.next;
-                l1=l1.next;
-              }
-            else
-            {
-               l.next=l2;
-                l=l.next; 
-                l2=l2.next;
-            }
+        {
+            tra.next=l1;
+                tra=tra.next;
+            l1=l1.next;
+        }
+        else
+        {
+            tra.next=l2;
+            tra=tra.next;
+            l2=l2.next;
+        }
         }
         while(l1!=null)
         {
-            l.next=l1;
-                l=l.next;
-                l1=l1.next;
+            tra.next=l1;
+                tra=tra.next;
+            l1=l1.next;
         }
         while(l2!=null)
         {
-             l.next=l2;
-                l=l.next; 
-                l2=l2.next;
+            tra.next=l2;
+            tra=tra.next;
+            l2=l2.next;
         }
-        l.next=null;
-        return ans.next;
+        tra.next=null;
+        return ans;
     }
 }
