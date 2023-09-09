16edb134c24dcc2cff955cb2cd4a3c7a6eb36178
fazalsidd <92528154+fazalsidd@users.noreply.github.com>
2023-09-09 12:48:12 +0530

    Time: 81 ms (10.20%), Space: 44.6 MB (8.22%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,27 +1,25 @@
 class Solution {
-    int flag=0;
-    public int helper(int[] coins, int amount,int dp[])
+    public int helper(int[] coins, int a,int j,int dp[][])
     {
-        if(amount==0)
-            {
-            flag=1;
-            return 0;}
-        if(amount<0)
-            return 1000;
-        if(dp[amount]!=Integer.MAX_VALUE)
-            return dp[amount];
-        for(int i=0;i<coins.length;i++)
+        if(a==0)
+            return 0;
+        if(dp[a][j]!=-1)
+            return dp[a][j];
+        int ans=100000;
+        for(int i=j;i<coins.length;i++)
         {
-            dp[amount]=Math.min(dp[amount],1+helper(coins,amount-coins[i],dp));
+            if(a-coins[i]>=0)
+            {
+                ans=Math.min(ans,1+helper(coins,a-coins[i],i,dp));
+            }
         }
-        return dp[amount];
+        return dp[a][j]=ans;
+        
     }
-    public int coinChange(int[] coins, int amount) {
-        int dp[]=new int[amount+1];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        int ans=helper(coins,amount,dp);
-        if(flag==0)
-            return -1;
-        return ans;
+    public int coinChange(int[] coins, int a) {
+        int dp[][]=new int[a+1][coins.length];
+        Arrays.stream(dp).forEach(b -> Arrays.fill(b, -1));
+        int ans=helper(coins,a,0,dp);
+        return ans>=100000?-1:ans;
     }
 }
