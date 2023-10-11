8879173e42a1c9e04a76dda01e79fd3af5a4cb15
fazalsidd <92528154+fazalsidd@users.noreply.github.com>
2023-10-11 22:21:23 +0530

    Time: 56 ms (75.00%), Space: 74.5 MB (19.23%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.java
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.java
@@ -1,54 +1,28 @@
 class Solution {
-    public int findmax(int arr1[],int p){
-        int s=0;
-        int e=arr1.length-1;
-        int ans=-1;
-        while(s<=e){
-            int mid=(s+e)/2;
-            if(arr1[mid]<=p){
-                ans=mid;
-                s=mid+1;
-            }
-            else{
-                e=mid-1;
-            }
+    public int[] fullBloomFlowers(int[][] f, int[] p) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        int ans[]=new int[p.length];
+        int temp[][]=new int[p.length][2];
+        for(int i=0;i<p.length;i++)
+        {
+            temp[i][0]=p[i];
+            temp[i][1]=i;
         }
-        return ans+1;
-    }
-    public int findright(int arr1[],int p){
-        int s=0;
-        int e=arr1.length-1;
-        int ans=-1;
-        while(s<=e){
-            int mid=(s+e)/2;
-            if(arr1[mid]<p){
-                ans=mid;
-                s=mid+1;
+         Arrays.sort(temp, (a, b) -> Integer.compare(a[0],b[0]));
+         Arrays.sort(f, (a, b) -> Integer.compare(a[0],b[0]));
+        int k=0;
+        for(int i=0;i<p.length;i++)
+        {
+            while(k<f.length&&f[k][0]<=temp[i][0])
+            {
+                pq.add(f[k][1]);
+                k++;
             }
-            else{
-                e=mid-1;
+            while(!pq.isEmpty()&&pq.peek()<temp[i][0])
+            {
+                pq.remove();
             }
-        }
-        return ans+1;
-    }
-   
-    public int[] fullBloomFlowers(int[][] flowers, int[] people) {
-        int n=flowers.length;
-        int arr1[]=new int[n];
-        int arr2[]=new int[n];
-        for(int i=0;i<n;i++){
-            arr1[i]=flowers[i][0];
-            arr2[i]=flowers[i][1];
-        }
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        int ans[]=new int[people.length];
-        for(int i=0;i<people.length;i++){
-            int p=people[i];
-            int chk1=findmax(arr1,p);
-            int chk2=findright(arr2,p);
-            ans[i]=chk1-chk2;
-
+            ans[temp[i][1]]=pq.size();
         }
         return ans;
     }
