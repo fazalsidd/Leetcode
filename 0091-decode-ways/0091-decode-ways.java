77e9a4ff1dd69237510ac1c02a576de2450a66a1
fazalsidd <92528154+fazalsidd@users.noreply.github.com>
2024-01-22 18:03:02 +0530

    Time: 1 ms (79.37%), Space: 42 MB (22.25%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -3,20 +3,21 @@ class Solution {
     {
         if(i>=s.length())
             return 1;
-        if(Character.getNumericValue(s.charAt(i))==0)
+        if(s.charAt(i)=='0')
             return 0;
         if(dp[i]!=-1)
             return dp[i];
-        int a=helper(i+1,s,dp);
-        if(i==s.length()-1)
-            return dp[i]=a;
-        int b=Character.getNumericValue(s.charAt(i))*10+Character.getNumericValue(s.charAt(i+1));
-        if(b>26)
-            return dp[i]=a;
-        
-        return dp[i]=a+helper(i+2,s,dp);
+        int ans=0;
+        ans+=helper(i+1,s,dp);
+        if(i<s.length()-1)
+        {
+            int n=Integer.parseInt(s.substring(i,i+2));
+            if(n<=26)
+                ans+=helper(i+2,s,dp);
+        }
+        return dp[i]=ans;
     }
-    public int numDecodings(String s){
+    public int numDecodings(String s) {
         int dp[]=new int[s.length()];
         Arrays.fill(dp,-1);
         return helper(0,s,dp);
