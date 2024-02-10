9b2945ce07e0d4020ad6d202c46df8dfd174cbe5
fazalsidd <92528154+fazalsidd@users.noreply.github.com>
2024-02-10 21:16:26 +0530

    Time: 14 ms (75.52%), Space: 43 MB (63.45%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,32 +1,39 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        int n=nums.length;
+        int index=0;
+        int max=1;
+        int dp[][]=new int[nums.length][2];
+        dp[0][0]=1;
+        dp[0][1]=-1;
+        ArrayList<Integer> ans=new ArrayList<>();
         Arrays.sort(nums);
-       List<Integer> ans=new ArrayList<>();
-       int[] dp=new int[n];
-       
-       Arrays.fill(dp,1);
-      int maxsize=1, idx=0;
-      for(int i=1;i<n;i++){
-          for(int j=0;j<i;j++){
-              if(nums[i]%nums[j]==0){
-                  dp[i]=Math.max(dp[i],dp[j]+1);
-                  if(dp[i]>maxsize){
-                      maxsize=dp[i];
-                      idx=i;
-                  }
-              }
-          }
-      }
-    int num=nums[idx];
-    for(int i=idx;i>=0;i--){
-        if(num%nums[i]==0 && dp[i]==maxsize){
-            ans.add(nums[i]);
-            num=nums[i];
-            maxsize--;
+        for(int i=1;i<nums.length;i++)
+        {
+            dp[i][0]=1;
+            dp[i][1]=-1;
+            for(int j=i-1;j>=0;j--)
+            {
+                if(nums[i]%nums[j]==0)
+                {
+                    if(dp[j][0]+1>dp[i][0])
+                    {
+                       dp[i][0]=dp[j][0]+1; 
+                        dp[i][1]=j;
+                    }
+                }
+            }
+            if(dp[i][0]>max)
+            {
+                max=dp[i][0];
+                index=i;
+            }
         }
-    }
-       
-     return ans;
-    }
+        while(dp[index][1]!=-1)
+        {
+            ans.add(nums[index]);
+            index=dp[index][1];
+        }
+        ans.add(nums[index]);
+        return ans;
+        }
 }
