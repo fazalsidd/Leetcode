866787816cf40970e032f00399667d8de5d2e14d
fazalsidd <92528154+fazalsidd@users.noreply.github.com>
2024-03-23 17:29:26 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (72.99%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,15 +3,15 @@ class Solution {
         if(nums.length==1)
             return nums[0];
         if(nums.length==2)
-            return Math.max(nums[0],nums[1]);
+          return  Math.max(nums[0],nums[1]);
+        
         int dp[]=new int[nums.length];
         dp[0]=nums[0];
-        dp[1]=nums[1];
+        dp[1]=Math.max(nums[1],nums[0]);
         for(int i=2;i<nums.length;i++)
         {
-            for(int j=0;j<i-1;j++)
-            dp[i]=Math.max(dp[i],nums[i]+dp[j]);
+            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
         }
-        return Math.max(dp[nums.length-1],dp[nums.length-2]);
+        return dp[nums.length-1];
     }
 }
