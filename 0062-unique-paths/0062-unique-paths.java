85cafb0b692985e3598c2691caf6a2ff9af1d7ec
fazalsidd <92528154+fazalsidd@users.noreply.github.com>
2024-03-28 14:51:52 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (84.45%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -5,10 +5,14 @@ class Solution {
         {
             for(int j=n-1;j>=0;j--)
             {
-                if(j==n-1||i==m-1)
-                    dp[i][j]=1;
+                if(i==m-1||j==n-1)
+                {
+                   dp[i][j]=1; 
+                }
                 else
+                {
                     dp[i][j]=dp[i+1][j]+dp[i][j+1];
+                }
             }
         }
         return dp[0][0];
