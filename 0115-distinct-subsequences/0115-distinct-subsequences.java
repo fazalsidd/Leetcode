219497cc43ce7a7eacd33c20fa4edfad5904488d
fazalsidd <92528154+fazalsidd@users.noreply.github.com>
2024-03-29 16:01:39 +0530

    Time: 18 ms (16.30%), Space: 49.2 MB (23.30%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,25 +1,45 @@
 class Solution {
-    public int helper(String s, String t,int i,int j,int dp[][])
-    {
-        if(j>=t.length())
-            return 1;
-        if(i>=s.length())
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
+//     public int helper(String s, String t,int i,int j,int dp[][])
+//     {
+//         if(j>=t.length())
+//             return 1;
+//         if(i>=s.length())
+//             return 0;
+//         if(dp[i][j]!=-1)
+//             return dp[i][j];
         
-        int exc=helper(s,t,i+1,j,dp);
-        int inc=0;
-        if(s.charAt(i)==t.charAt(j))
-            inc=helper(s,t,i+1,j+1,dp);
+//         int exc=helper(s,t,i+1,j,dp);
+//         int inc=0;
+//         if(s.charAt(i)==t.charAt(j))
+//             inc=helper(s,t,i+1,j+1,dp);
         
-        return dp[i][j]=inc+exc;
+//         return dp[i][j]=inc+exc;
             
-    }
+//     }
     public int numDistinct(String s, String t) {
-        int dp[][]=new int[s.length()+1][t.length()+1];
-        Arrays.stream(dp).forEach(a -> Arrays.fill(a, -1));
-        return helper(s,t,0,0,dp);
+        int dp[][]=new int[t.length()+1][s.length()+1];
+        for(int i=t.length();i>=0;i--)
+        {
+            for(int j=s.length();j>=0;j--)
+            {
+                if(i==t.length())
+                    dp[i][j]=1;
+                else if(j==s.length())
+                    dp[i][j]=0;
+                else
+                {
+                    if(s.charAt(j)==t.charAt(i))
+                    {
+                        dp[i][j]=dp[i+1][j+1]+dp[i][j+1];
+                    }
+                    else
+                    {
+                        dp[i][j]=dp[i][j+1];
+                    }
+                }
+            }
+        }
+        return dp[0][0];
         
     }
 }
